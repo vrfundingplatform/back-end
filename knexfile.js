6dbee2e5721550c0a6ed7ff917dee731571d3ef4
DeniseFafette <denisefafette@gmail.com>
2020-02-03 21:44:50 -0800

    updated knexfile.js with production

--- knexfile.js
+++ knexfile.js
@@ -16,4 +16,25 @@ module.exports = {
   },
     seeds: { directory: './data/seeds' },
   },
+
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: { filename: './data/vrfund.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    pool: {
+      afterCreate:(conn, done) => {
+          conn.run('PRAGMA foreign_keys = ON', done);
+      }
+  },
+    seeds: { directory: './data/seeds' },
+  },
 };
+
+
+
+
