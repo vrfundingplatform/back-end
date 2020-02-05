8d9ec74efe6fb66b7d8c6b1b5ebe21d3490c9e3f
DeniseFafette <denisefafette@gmail.com>
2020-02-05 11:09:37 -0800

    updated migrations db3 and projectsRouter

---
+++ data/migrations/20200205103754_vrfund.js
@@ -0,0 +1,83 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments('id')
+          .notNullable()
+          .unique()
+  
+        users.string('username', 255)
+          .notNullable()
+          .unique()
+  
+        users.string('password', 255)
+          .notNullable()
+  
+        users.string('email', 255)
+          .notNullable()
+          .unique()
+      
+        users.string('firstname', 255)
+          .notNullable()
+  
+        users.string('lastname', 255)
+          .notNullable()
+          
+        users.string('country', 255)
+          .notNullable()
+  
+        users.string('state', 255)
+  
+        users.string('avatar', 255)
+  
+        users.text('bio', 255)
+  
+        users.boolean('bankacct')
+          .notNullable()
+          .defaultTo(false)
+  
+        users.boolean('age')
+          .notNullable()
+          .defaultTo(false)
+    })
+  
+    .createTable('projects', projects => {
+        projects.increments('id')
+          .notNullable()
+          .unique()
+  
+        projects.integer('users_projectid', 255)
+          .notNullable()
+          .unique()
+  
+        projects.string('category', 255)
+          .notNullable()
+  
+        projects.string('subcategory', 255)
+      
+        projects.string('status', 255)
+          .notNullable()
+  
+        projects.string('title', 255)
+          .notNullable()
+          
+        projects.string('startDate', 255)
+        .notNullable()
+  
+        projects.string('endDate', 255)
+        .notNullable()
+  
+        projects.string('cta', 255)
+  
+        projects.text('desc', 255)
+  
+        projects.integer('goal')
+        .notNullable()
+  
+        projects.string('primaryPic')
+    })
+  };
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('projects')
+    .dropTableIfExists('users')
+};
