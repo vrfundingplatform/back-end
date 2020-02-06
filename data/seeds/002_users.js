69da21d261f922e7d6e2585d8b90b32fa88e1dc6
Kevin Smith <keveightysev@gmail.com>
2020-02-06 15:06:51 -0600

    test server

--- data/seeds/002_users.js
+++ data/seeds/002_users.js
@@ -1,24 +1,30 @@
+const bcrypt = require("bcryptjs");
+
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("users")
+  return knex("projects")
     .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("users").insert([
-        {
-          id: 1,
-          username: "bslayer",
-          password: "test1",
-          email: "slayer@gmail.com",
-          firstname: "Buffy",
-          lastname: "Summers",
-          country: "United States",
-          state: "California",
-          avatar: "",
-          bio: "Seeks out and destroy vampires!",
-          bankacct: true,
-          age: true
-        }
-      ]);
+    .then(() => {
+      return knex("users")
+        .del()
+        .then(function() {
+          // Inserts seed entries
+          return knex("users").insert([
+            {
+              id: 1,
+              username: "bslayer",
+              password: bcrypt.hashSync("test1", 8),
+              email: "slayer@gmail.com",
+              firstname: "Buffy",
+              lastname: "Summers",
+              country: "United States",
+              state: "California",
+              avatar: "",
+              bio: "Seeks out and destroy vampires!",
+              bankacct: true,
+              age: true
+            }
+          ]);
+        });
     });
 };
